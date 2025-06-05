101bba2bda3383e469bfec1618e558ba213d1f65
vitmonjo <vitmonjo@gmail.com>
2025-06-04 22:01:45 -0300

    Update repository with findByClientId

--- ClientManagement-backend/src/main/java/com/example/ClientManagement/Contact/ContactRepository.java
+++ ClientManagement-backend/src/main/java/com/example/ClientManagement/Contact/ContactRepository.java
@@ -1,6 +1,8 @@
 package com.example.ClientManagement.Contact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ContactRepository extends JpaRepository<Contact, String> {
+    List<Contact> findByClientId(String clientId);
 }
